98aae677eac3dd4d1536f0470732622a38517cf7
Amanda Punsammy <amanda.punsammy@gmail.com>
2018-12-29 13:00:12 -0500

    initial setup and commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Import library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// create a Component
+const App = () => (
+  <Header />
+);
+
+// render to device
+AppRegistry.registerComponent('albums', () => App);
